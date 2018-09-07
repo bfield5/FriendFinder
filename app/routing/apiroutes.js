2e807f76ec617745a7491269b1fa34af47848834
Brian Field <brianfield74@yahoo.com>
2018-09-07 12:00:53 -0600

    .

--- app/routing/apiroutes.js
+++ app/routing/apiroutes.js
@@ -1,9 +1,9 @@
- // ROUTES
+// ROUTES
 
- app.get("/api/friends", function(req, res) {
-    res.sendFile(path.join(__dirname, "../data/friends.js"));
-  });
+app.get("/api/friends", function (req, res) {
+  res.sendFile(path.join(__dirname, "../data/friends.js"));
+});
 
-  app.post("/api/friends", function(req, res) {
-    res.sendFile(path.join(__dirname, "friends.js"));
-  });
+app.post("/api/friends", function (req, res) {
+  res.sendFile(path.join(__dirname, "friends.js"));
+});
--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+var PORT = process.env.PORT || 3000;
+
+
+
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
