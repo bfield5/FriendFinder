9d3ddd2e110dde1c3993b232f8ed90fc92660a87
Brian Field <brianfield74@yahoo.com>
2018-09-07 11:40:06 -0600

    .

---
+++ app/routing/apiroutes.js
@@ -0,0 +1,9 @@
+ // ROUTES
+
+ app.get("/api/friends", function(req, res) {
+    res.sendFile(path.join(__dirname, "../data/friends.js"));
+  });
+
+  app.post("/api/friends", function(req, res) {
+    res.sendFile(path.join(__dirname, "friends.js"));
+  });
