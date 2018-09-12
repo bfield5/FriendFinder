644337a50e9a45c1400f41d5fec798db1c99aeb7
Brian Field <brianfield74@yahoo.com>
2018-09-12 09:11:16 -0600

    .

--- app/routing/apiroutes.js
+++ app/routing/apiroutes.js
@@ -11,7 +11,7 @@ var friends = require("../data/friends.js");
 module.exports = function(app) {
   // Return all friends found in friends.js as JSON
   app.get("/api/friends", function(req, res) {
-    return res.json(friends);
+    res.json(friends);
   });
 
   app.post("/api/friends", function(req, res) {
--- server.js
+++ server.js
@@ -11,12 +11,11 @@ app.use(express.static(__dirname + "/app/css"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
 
 // API and HTML routes
 require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
-
 
 
 
