f85c1fc69de363ac4af9bd8bfd1f54fef89f366f
Brian Field <brianfield74@yahoo.com>
2018-09-12 02:14:48 -0600

    .

--- app/routing/htmlroutes.js
+++ app/routing/htmlroutes.js
@@ -13,17 +13,17 @@
 
 // ROUTES
  
-var path = require('path');
+var path = require("path");
 
 // Export HTML routes
 module.exports = function(app) {
 	// Home page
-	app.get('/', function(req, res) {
-		res.sendFile(path.join(__dirname, '../public/home.html'));
+	app.get("/", function(req, res) {
+		res.sendFile(path.join(__dirname, "../public/home.html"));
 	});
 
 	// Survey page
-	app.get('/survey', function(req, res) {
-		res.sendFile(path.join(__dirname, '../public/survey.html'));
+	app.get("/survey", function(req, res) {
+		res.sendFile(path.join(__dirname, "../public/survey.html"));
 	});
 };
